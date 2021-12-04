90e261d28f75e6bb0374ba020e5b3a3a47803a29
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2021-12-04 16:03:17 +0700

    day03: Revamp part 1

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -8,57 +8,73 @@ use itertools::{Itertools, MinMaxResult};
 
 use aoc2021::argparser;
 
+type BitVec = Vec<bool>;
+
 fn main() {
     let input_file = std::env::args().nth(1).unwrap_or_else(|| "-".into());
     let reader = argparser::reader_from_file(input_file).expect("cannot open file");
     let input = parse_input(reader).expect("cannot parse input");
 
-    let p1_answer = compute_power_consumption(input.as_ref()).unwrap();
+    let p1_answer = compute_power_consumption(input.as_slice()).unwrap();
     println!("Part 1 answer: {}", p1_answer);
 }
 
-fn parse_input<R: BufRead>(reader: R) -> anyhow::Result<Vec<String>> {
-    let data = reader
+fn parse_input<R: BufRead>(reader: R) -> anyhow::Result<Vec<BitVec>> {
+    reader
         .lines()
-        .map(|line| Ok(line?.trim().to_string()))
-        .collect::<anyhow::Result<Vec<_>>>();
-    data
+        .map(|line| {
+            line?
+                .trim()
+                .bytes()
+                .map(|c| match c {
+                    b'0' => Ok(false),
+                    b'1' => Ok(true),
+                    _ => bail!("invalid character in bitstring: {}", c),
+                })
+                .collect::<anyhow::Result<BitVec>>()
+        })
+        .collect()
 }
 
-/// Compute the power consumption, which is the product of these two factors:
+/// Computes the power consumption, which is the product of these two factors:
 /// - gamma = radix majority voting of bit vector data
 /// - epsilon = radix minority voting of bit vector data
-fn compute_power_consumption(data: &[String]) -> anyhow::Result<usize> {
-    let bit_length_minmax = data.iter().map(|v| v.as_bytes().len()).minmax();
-    let bit_length = match bit_length_minmax {
-        MinMaxResult::NoElements => bail!("cannot compute gamma/epsilon of empty data"),
-        MinMaxResult::MinMax(p, q) if p != q => {
-            bail!("data are of non-uniform length (between {} and {})", p, q)
-        }
-        MinMaxResult::MinMax(p, _) | MinMaxResult::OneElement(p) => p,
-    };
-    let zipped_result: anyhow::Result<Vec<_>> = (0..bit_length)
+fn compute_power_consumption(data: &[BitVec]) -> anyhow::Result<usize> {
+    let bit_length = common_bit_length(data)?;
+    let zipped_result = (0..bit_length)
         .map(|i| {
-            let tug_result: anyhow::Result<isize> = data
-                .iter()
-                .map(|v| match v.as_bytes()[i] {
-                    b'0' => Ok(-1),
-                    b'1' => Ok(1),
-                    c => bail!("unrecognized character: {}", c as char),
-                })
-                .sum();
-            match tug_result?.cmp(&0) {
-                Ordering::Greater => Ok(('1', '0')),
-                Ordering::Less => Ok(('0', '1')),
-                Ordering::Equal => bail!("cannot decide strict majority/minority"),
+            let tug_result: isize = data.iter().map(|v| if v[i] { 1 } else { -1 }).sum();
+            match tug_result.cmp(&0) {
+                Ordering::Greater => Ok((true, false)),
+                Ordering::Less => Ok((false, true)),
+                Ordering::Equal => bail!("cannot decide strict majority or minority"),
             }
         })
-        .collect();
-    // TODO: lazily unzip iterator of 2-tuples into 2-tuple of vectors
-    let (gamma, epsilon): (String, String) = zipped_result?.iter().copied().unzip();
-    let gamma = usize::from_str_radix(gamma.as_str(), 2)?;
-    let epsilon = usize::from_str_radix(epsilon.as_str(), 2)?;
+        .collect::<anyhow::Result<Vec<_>>>();
+
+    let (gamma, epsilon): (BitVec, BitVec) = zipped_result?.iter().copied().unzip();
+    let gamma = bitvec_to_integer(&gamma);
+    let epsilon = bitvec_to_integer(&epsilon);
     Ok(gamma * epsilon)
+}
+
+/// Computes the common bit length among the collection of bit vectors.
+/// An error occurs if they disagree.
+fn common_bit_length(data: &[BitVec]) -> anyhow::Result<usize> {
+    let minmax_result = data.iter().map(|v| v.len()).minmax();
+    match minmax_result {
+        MinMaxResult::NoElements => bail!("empty collection of bit vectors"),
+        MinMaxResult::MinMax(min, max) if min != max => {
+            bail!("non-uniform length bit vectors between {} and {}", min, max)
+        }
+        MinMaxResult::MinMax(l, _) | MinMaxResult::OneElement(l) => Ok(l),
+    }
+}
+
+/// Calculates the integer representation of the given bit vector in MSB-first order.
+#[allow(clippy::ptr_arg)]
+fn bitvec_to_integer(s: &BitVec) -> usize {
+    s.iter().fold(0, |acc, val| 2 * acc + (*val) as usize)
 }
 
 /// Compute the life support rating, which is the product of these two factors:
