ebdbd6a474fd084100adc5b7bddba07a8ebcbbbc
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2021-12-05 01:23:40 +0700

    day04: Prepare for new day

---
+++ src/bin/day04.rs
@@ -0,0 +1,14 @@
+//! Day 4: Giant Squid, Advent of Code 2021
+//! https://adventofcode.com/2021/day/4
+use aoc2021::argparser;
+
+fn main() {
+    let input_src = argparser::InputSrc::from_arg(std::env::args().nth(1).as_deref());
+    let _input_reader = input_src.create_reader().expect("cannot open file");
+
+    let p1_answer = 0;
+    println!("Part 1 answer: {}", p1_answer);
+
+    let p2_answer = 0;
+    println!("Part 2 answer: {}", p2_answer);
+}
