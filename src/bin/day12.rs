9dcc531b3e05d04fc7182e717a99514947583e93
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2021-12-12 22:43:45 +0700

    day12: Completed with nice usage of simple closures refactoring

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::io::BufRead;
 
 use anyhow::Context;
+use itertools::Itertools;
 
 use aoc2021::argparser;
 use aoc2021::try_collect::TryCollectArray;
@@ -13,12 +14,52 @@ fn main() {
     let input_reader = input_src.get_reader().expect("cannot open file");
     let graph = parse_input(input_reader).expect("cannot parse input");
 
-    // Part 1: exhaustive search for all paths from start to end
-    let p1_answer = graph.exhaustive_paths("start", "end");
+    // Part 1: visiting each small cave at most once
+    let p1_answer = {
+        let mut count: usize = 0;
+        graph.exhaustive_traverse(
+            "start",
+            "end",
+            // Acceptable cases:
+            // 1.  the next node is a big cave (containing uppercase letters), or
+            // 2.  the path so far does _not_ contain such next node
+            |next, path| next.chars().any(char::is_uppercase) || !path.contains(&next),
+            |_path| {
+                // eprintln!("=> {}", _path.join(", "));
+                count += 1
+            },
+        );
+        count
+    };
     println!("Part 1 answer: {}", p1_answer);
 
-    // Part 2: TODO
-    let p2_answer: usize = 0;
+    // Part 2: visiting each small cave at most once, except for one that is allowed up to twice
+    //         but excluding the start and the end
+    let p2_answer = {
+        let mut count = 0;
+        graph.exhaustive_traverse(
+            "start",
+            "end",
+            // Acceptable cases (the first two are the same as part 1):
+            // 1.  the next node is a big cave (containing uppercase letters), or
+            // 2.  the path so far does _not_ contain such next node, or
+            // 3.  the next node does _not_ go back to "start" and all previous small caves are unique visits
+            |next, path| {
+                next.chars().all(char::is_uppercase)
+                    || !path.contains(&next)
+                    || next.ne("start")
+                        && path
+                            .iter()
+                            .filter(|prev| !prev.chars().all(char::is_uppercase))
+                            .all_unique()
+            },
+            |_path| {
+                // eprintln!("=> {}", _path.join(", "));
+                count += 1
+            },
+        );
+        count
+    };
     println!("Part 2 answer: {}", p2_answer);
 }
 
@@ -61,11 +102,24 @@ impl Graph {
     fn add_edge<T: AsRef<str>>(&mut self, u: T, v: T) {
         let u = u.as_ref().to_string();
         let v = v.as_ref().to_string();
-        self.adjlists.entry(u).or_insert_with(|| Vec::new()).push(v);
+        self.adjlists.entry(u).or_insert_with(Vec::new).push(v);
     }
 
-    /// Exhaustive path searching from start to end.
-    fn exhaustive_paths<T: AsRef<str>>(&self, start: T, end: T) -> usize {
+    /// Exhaustive path searching from `start` to `end`.
+    /// Before walking onto each next node, the predicate `decide_should_walk` decides
+    /// whether to proceed based on the next node and the path walked so far from the `start`.
+    /// For each time a finished path from `start` to `end` is found,
+    /// the function `process_finished_path` is invoked on such path for further processing.
+    fn exhaustive_traverse<T: AsRef<str>, P, F>(
+        &self,
+        start: T,
+        end: T,
+        mut decide_should_walk: P,
+        mut process_finished_path: F,
+    ) where
+        P: FnMut(&str, &[&str]) -> bool,
+        F: FnMut(&[&str]),
+    {
         #[derive(Debug, Eq, PartialEq)]
         enum Event<'a> {
             PreStack(&'a str),
@@ -73,30 +127,28 @@ impl Graph {
         }
         let start = start.as_ref();
         let end = end.as_ref();
-        let mut count = 0;
 
         let mut event_stack = Vec::from([Event::PreStack(start)]);
         let mut depth_stack = Vec::new();
         while let Some(event) = event_stack.pop() {
             match event {
-                Event::PreStack(curr) if curr == end => {
-                    eprintln!("{:?}", depth_stack);
-                    count += 1;
-                }
                 Event::PreStack(curr) => {
                     event_stack.push(Event::InStack(curr));
                     depth_stack.push(curr);
-                    for next in self.adjlists[curr].iter() {
-                        if next.chars().all(char::is_uppercase) || !depth_stack.contains(&&**next) {
-                            event_stack.push(Event::PreStack(next))
+                    if curr == end {
+                        process_finished_path(depth_stack.as_slice());
+                    } else {
+                        for next in self.adjlists[curr].iter() {
+                            if decide_should_walk(next, depth_stack.as_slice()) {
+                                event_stack.push(Event::PreStack(next))
+                            }
                         }
                     }
                 }
-                Event::InStack(_curr) => {
-                    depth_stack.pop();
+                Event::InStack(curr) => {
+                    assert_eq!(curr, depth_stack.pop().expect("must not be empty"));
                 }
             }
         }
-        count
     }
 }
