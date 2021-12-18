30ac4e15e40a632f5ca63d06dcaac3298102a44d
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2021-12-19 00:29:23 +0700

    day14: Completed

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,9 +1,11 @@
 //! Day 14: Extended Polymerization, Advent of Code 2021  
 //! <https://adventofcode.com/2021/day/14>
+use std::collections::HashMap;
 use std::io::BufRead;
 use std::str::FromStr;
 
-use anyhow::{anyhow, bail, Context};
+use anyhow::{bail, Context};
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -20,22 +22,42 @@ fn main() {
         ins_rules,
     } = Input::from_buffer(input_reader).expect("cannot parse input");
 
-    eprintln!("{:?}", template);
-    eprintln!("{:?}", ins_rules);
+    // Save first and last chars for reconciliation later
+    let first = template.chars().next().expect("empty template string");
+    let last = template.chars().last().unwrap();
 
-    // Part 1: TODO
-    let p1_answer = 0;
-    println!("Part 1 answer: {}", p1_answer);
+    // Count bigrams or the original template polymer
+    let bigram_counts = template.chars().tuple_windows::<(_, _)>().counts();
 
-    // Part 2: TODO
-    let p2_answer = 0;
-    println!("Part 2 answer: {}", p2_answer);
+    // Part 1: Applying insertion rules 10 times
+    let bigram_counts = (0..10).fold(bigram_counts, |counts, _| {
+        next_polymer_bigram_counts(&counts, ins_rules.as_slice())
+    });
+    let p1_diff = {
+        let unigram_counts = unigrams_from_bigrams(first, last, &bigram_counts);
+        let (&min_count, &max_count) = unigram_counts.values().minmax().into_option().unwrap();
+        max_count - min_count
+    };
+    println!("Part 1 answer: {}", p1_diff);
+
+    // Part 2: Apply insertion rules 30 more times
+    let bigram_counts = (0..30).fold(bigram_counts, |counts, _| {
+        next_polymer_bigram_counts(&counts, ins_rules.as_slice())
+    });
+    let p2_diff = {
+        let unigram_counts = unigrams_from_bigrams(first, last, &bigram_counts);
+        let (&min_count, &max_count) = unigram_counts.values().minmax().into_option().unwrap();
+        max_count - min_count
+    };
+    println!("Part 2 answer: {}", p2_diff);
 }
 
 /// Program input data
 #[derive(Debug, Clone)]
 struct Input {
+    /// Starting polymer template as a sequence of characters
     template: String,
+    /// Collection of insertion rules
     ins_rules: Vec<InsertionRule>,
 }
 
@@ -63,11 +85,14 @@ impl Input {
     }
 }
 
+/// Bigram: a consecutive pair of characters
+type Bigram = (char, char);
+
 /// Polymerization insertion rules
 #[derive(Debug, Clone)]
 struct InsertionRule {
     /// Pair of characters to capture
-    pattern: (char, char),
+    pattern: Bigram,
     /// Character to insert between the pair of pattern characters
     insert_char: char,
 }
@@ -82,7 +107,7 @@ impl FromStr for InsertionRule {
         // let RE: Regex = Regex::new(r"\s*(\w\w)\s+->\s+(\w)\s*").unwrap();
         let captures = RE
             .captures(s)
-            .ok_or_else(|| anyhow!("invalid insertion rule: {}", s))?;
+            .with_context(|| format!("invalid insertion rule: {}", s))?;
         let [fst, snd] = captures[1].chars().try_collect_exact_array()?;
         let [insert_char] = captures[2].chars().try_collect_exact_array()?;
         let pattern = (fst, snd);
@@ -91,4 +116,42 @@ impl FromStr for InsertionRule {
             insert_char,
         })
     }
+}
+
+/// Computes the bigram counts of the next polymer obtained by transforming the input polymer
+/// (whose bigram counts is given as input) based on `insertion_rules`.
+fn next_polymer_bigram_counts(
+    bigram_counts: &HashMap<Bigram, usize>,
+    insertion_rules: &[InsertionRule],
+) -> HashMap<Bigram, usize> {
+    let mut next_bigram_counts = HashMap::new();
+    for rule in insertion_rules {
+        let count = bigram_counts.get(&rule.pattern).copied().unwrap_or(0);
+        if count == 0 {
+            continue;
+        }
+        *next_bigram_counts
+            .entry((rule.pattern.0, rule.insert_char))
+            .or_insert(0) += count;
+        *next_bigram_counts
+            .entry((rule.insert_char, rule.pattern.1))
+            .or_insert(0) += count;
+    }
+    next_bigram_counts
+}
+
+/// Counts individual elements based on bigram counts of a polymer.
+fn unigrams_from_bigrams(
+    first: char,
+    last: char,
+    bigram_counts: &HashMap<Bigram, usize>,
+) -> HashMap<char, usize> {
+    let mut unigram_counts = HashMap::from([(first, 1), (last, 1)]);
+    for (bigram, count) in bigram_counts.iter() {
+        *unigram_counts.entry(bigram.0).or_insert(0) += count;
+        *unigram_counts.entry(bigram.1).or_insert(0) += count;
+    }
+    // Undo double counting
+    unigram_counts.values_mut().for_each(|v| *v /= 2);
+    unigram_counts
 }
