aeb6048a20257b72ca2e3d5be42d1b7fe99af406
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2021-12-23 15:59:53 +0700

    day16: Done

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -3,10 +3,12 @@
 use std::collections::VecDeque;
 use std::io::Read;
 
-use anyhow::{ensure, Context};
+use anyhow::{bail, ensure, Context};
+use itertools::Itertools;
 use num::PrimInt;
 
 use aoc2021::argparser;
+use aoc2021::collect_array::CollectArray;
 
 /// Main program
 fn main() {
@@ -16,15 +18,15 @@ fn main() {
 
     // Parses the packet from the input stream
     let (_, packet) = Packet::from_stream(&mut input_stream).expect("cannot parse packet");
-    eprintln!("{:?}", packet);
+    // eprintln!("{:?}", packet);
 
     // Part 1: Sum of version values of all packets
     let p1_answer = packet
         .reduce(&|subpacket, children| subpacket.version as u64 + children.iter().sum::<u64>());
     println!("Part 1 answer: {}", p1_answer);
 
-    // Part 2: TODO
-    let p2_answer = 0;
+    // Part 2: Evaluate the packet
+    let p2_answer = packet.eval().expect("error during evaluation");
     println!("Part 2 answer: {}", p2_answer);
 }
 
@@ -79,7 +81,6 @@ impl<R: Read> Iterator for InputStream<R> {
 #[derive(Debug, Clone)]
 struct Packet {
     version: u8,
-    type_id: u8,
     payload: Payload,
 }
 
@@ -92,14 +93,24 @@ impl Packet {
         let type_id = decimal_from_bits(stream.fetch::<3>()?.as_slice());
         let (nbits_read, payload) = match type_id {
             4 => Payload::parse_literal(stream)?,
-            _ => Payload::parse_ops(stream)?,
+            _ => Payload::parse_ops(stream, Operator::new(type_id)?)?,
         };
-        let packet = Packet {
-            version,
-            type_id,
-            payload,
-        };
+        let packet = Packet { version, payload };
         Ok((nbits_read + 6, packet))
+    }
+
+    /// Evaluates the expression described by the packet.
+    fn eval(&self) -> anyhow::Result<u64> {
+        match &self.payload {
+            Payload::Literal(value) => Ok(*value),
+            Payload::Operation(op, children) => {
+                let children: Vec<_> = children
+                    .iter()
+                    .map(|subpacket| subpacket.eval())
+                    .collect::<anyhow::Result<_>>()?;
+                op.apply(children.as_slice())
+            }
+        }
     }
 
     /// Reduces the packet tree structure into a single value.
@@ -113,7 +124,7 @@ impl Packet {
     {
         let children: Vec<_> = match &self.payload {
             Payload::Literal(_) => Vec::new(),
-            Payload::Operation(subpackets) => subpackets.iter().map(|sp| sp.reduce(func)).collect(),
+            Payload::Operation(_, children) => children.iter().map(|sp| sp.reduce(func)).collect(),
         };
         func(self, children.as_slice())
     }
@@ -122,10 +133,10 @@ impl Packet {
 /// Payload of the [`Packet`]
 #[derive(Debug, Clone)]
 enum Payload {
-    /// Payload of the [`Packet`] with `type_id == 4`, containing the literal value
+    /// Payload of [`Packet`] with `type_id == 4` containing the literal value
     Literal(u64),
-    /// Payload of the [`Packet`] with `type_id != 4`, containing sequence of sub-packets
-    Operation(Vec<Packet>),
+    /// Payload of [`Packet`] containing an operation on subpackets
+    Operation(Operator, Vec<Packet>),
 }
 
 impl Payload {
@@ -147,29 +158,32 @@ impl Payload {
 
     /// Parses [`Payload::Operation`] by consuming the next few bits from the stream.
     /// This method dispatches to subroutine depending on the length type ID being read next.
-    fn parse_ops<R: Read>(stream: &mut InputStream<R>) -> anyhow::Result<(usize, Payload)> {
+    fn parse_ops<R: Read>(
+        stream: &mut InputStream<R>,
+        op: Operator,
+    ) -> anyhow::Result<(usize, Payload)> {
         let [length_type_id] = stream.fetch()?;
-        let (nbits_read, payload) = match length_type_id {
-            0 => Payload::parse_ops_by_bit_length(stream)?,
-            1 => Payload::parse_ops_by_packet_count(stream)?,
+        let (nbits_read, children) = match length_type_id {
+            0 => Payload::parse_children_by_bit_length(stream)?,
+            1 => Payload::parse_children_by_packet_count(stream)?,
             _ => unreachable!(),
         };
-        Ok((nbits_read + 1, payload))
+        Ok((nbits_read + 1, Payload::Operation(op, children)))
     }
 
     /// Parses [`Payload::Operation`] by consuming the next few bits from the stream,
     /// already knowing that the length type ID previously read was 0.
     /// Hence, the next 15 bits indicate the total length in bits of sub-packets, etc.
-    fn parse_ops_by_bit_length<R: Read>(
+    fn parse_children_by_bit_length<R: Read>(
         stream: &mut InputStream<R>,
-    ) -> anyhow::Result<(usize, Payload)> {
+    ) -> anyhow::Result<(usize, Vec<Packet>)> {
         let target_length: usize = decimal_from_bits(stream.fetch::<15>()?.as_slice());
         let mut nbits_read = 0;
-        let mut subpackets = Vec::new();
+        let mut children = Vec::new();
         while nbits_read < target_length {
             let (nbits_more, subpacket) = Packet::from_stream(stream)?;
             nbits_read += nbits_more;
-            subpackets.push(subpacket);
+            children.push(subpacket);
         }
         ensure!(
             nbits_read == target_length,
@@ -177,24 +191,85 @@ impl Payload {
             nbits_read,
             target_length
         );
-        Ok((nbits_read + 15, Payload::Operation(subpackets)))
+        Ok((nbits_read + 15, children))
     }
 
     /// Parses [`Payload::Operation`] by consuming the next few bits from the stream,
     /// already knowing that the length type ID previously read was 1.
     /// Hence, the next 11 bits indicate the number of sub-packets.
-    fn parse_ops_by_packet_count<R: Read>(
+    fn parse_children_by_packet_count<R: Read>(
         stream: &mut InputStream<R>,
-    ) -> anyhow::Result<(usize, Payload)> {
+    ) -> anyhow::Result<(usize, Vec<Packet>)> {
         let subpacket_count: usize = decimal_from_bits(stream.fetch::<11>()?.as_slice());
         let mut nbits_read = 0;
-        let mut subpackets = Vec::with_capacity(subpacket_count);
+        let mut children = Vec::with_capacity(subpacket_count);
         for _ in 0..subpacket_count {
             let (nbits_more, subpacket) = Packet::from_stream(stream)?;
             nbits_read += nbits_more;
-            subpackets.push(subpacket);
+            children.push(subpacket);
         }
-        Ok((nbits_read + 11, Payload::Operation(subpackets)))
+        Ok((nbits_read + 11, children))
+    }
+}
+
+/// Packet in BITS transmission
+#[derive(Debug, Clone, Copy)]
+enum Operator {
+    /// Sum operator when packet's `type_id == 0`
+    Sum,
+    /// Product operator when packet's `type_id == 1`
+    Product,
+    /// Minimum operator when packet's `type_id == 2`
+    Minimum,
+    /// Maximum operator when packet's `type_id == 3`
+    Maximum,
+    /// Operator which returns `1` if the result of the first subpacket is greater than the second;
+    /// otherwise returns `0`. This indicates that packet's `type_id == 5`.
+    GreaterThan,
+    /// Operator which returns `1` if the result of the first subpacket is less than the second;
+    /// otherwise returns `0`. This indicates that packet's `type_id == 6`.
+    LessThan,
+    /// Operator which returns `1` if the result of the first subpacket is equal to the second;
+    /// otherwise returns `0`. This indicates that packet's `type_id == 7`.
+    EqualTo,
+}
+
+impl Operator {
+    fn new(type_id: u8) -> anyhow::Result<Self> {
+        Ok(match type_id {
+            0 => Operator::Sum,
+            1 => Operator::Product,
+            2 => Operator::Minimum,
+            3 => Operator::Maximum,
+            4 => unreachable!(),
+            5 => Operator::GreaterThan,
+            6 => Operator::LessThan,
+            7 => Operator::EqualTo,
+            _ => bail!("unknown type_id {}", type_id),
+        })
+    }
+
+    /// Applies the operation on the children.
+    fn apply(&self, children: &[u64]) -> anyhow::Result<u64> {
+        let children = children.iter().copied();
+        Ok(match self {
+            Operator::Sum => children.sum1().context("missing a child")?,
+            Operator::Product => children.product1().context("missing a child")?,
+            Operator::Maximum => children.max().context("missing a child")?,
+            Operator::Minimum => children.min().context("missing a child")?,
+            Operator::GreaterThan => {
+                let [fst, snd] = children.collect_exact_array()?;
+                (fst > snd) as u64
+            }
+            Operator::LessThan => {
+                let [fst, snd] = children.collect_exact_array()?;
+                (fst < snd) as u64
+            }
+            Operator::EqualTo => {
+                let [fst, snd] = children.collect_exact_array()?;
+                (fst == snd) as u64
+            }
+        })
     }
 }
 
