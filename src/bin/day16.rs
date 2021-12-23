6a39485402d55e3641e91cd0b6aa94ff92290650
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2021-12-23 14:00:52 +0700

    day16: Part 1 done

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,9 +1,9 @@
 //! Day N: PROBLEM NAME, Advent of Code 2021  
 //! <https://adventofcode.com/2021/day/N>
 use std::collections::VecDeque;
-use std::io::{Bytes, Read};
+use std::io::Read;
 
-use anyhow::Context;
+use anyhow::{ensure, Context};
 use num::PrimInt;
 
 use aoc2021::argparser;
@@ -18,8 +18,9 @@ fn main() {
     let (_, packet) = Packet::from_stream(&mut input_stream).expect("cannot parse packet");
     eprintln!("{:?}", packet);
 
-    // Part 1: TODO
-    let p1_answer = 0;
+    // Part 1: Sum of version values of all packets
+    let p1_answer = packet
+        .reduce(&|subpacket, children| subpacket.version as u64 + children.iter().sum::<u64>());
     println!("Part 1 answer: {}", p1_answer);
 
     // Part 2: TODO
@@ -30,14 +31,14 @@ fn main() {
 /// Alias for bit type (can either be 0 or 1)
 type Bit = u8;
 
-/// Program input data
+/// Wrapper over program input to provide the stream as an iterator
 struct InputStream<R: Read> {
-    source: Bytes<R>,
+    source: std::io::Bytes<R>,
     buffer: VecDeque<Bit>,
 }
 
 impl<R: Read> InputStream<R> {
-    /// Creates a new input stream object
+    /// Creates a new input stream from [`std::io::Read`] object
     fn new(reader: R) -> Self {
         InputStream {
             source: reader.bytes(),
@@ -45,7 +46,7 @@ impl<R: Read> InputStream<R> {
         }
     }
 
-    /// Fetches a few bits from the stream.
+    /// Fetches the next few bits from the stream and returns as an array.
     fn fetch<const SIZE: usize>(&mut self) -> anyhow::Result<[Bit; SIZE]> {
         let mut target = [0; SIZE];
         for element in target.iter_mut() {
@@ -64,7 +65,7 @@ impl<R: Read> Iterator for InputStream<R> {
                 Ok(c) => c as char,
                 Err(err) => return Some(Err(anyhow::Error::new(err))),
             };
-            let bits = match bits_from_hex_char(c) {
+            let bits = match bits_from_hex(c) {
                 Ok(bits) => bits,
                 Err(err) => return Some(Err(err)),
             };
@@ -82,25 +83,16 @@ struct Packet {
     payload: Payload,
 }
 
-/// Payload of the packet, depending on the `type_id`
-#[derive(Debug, Clone)]
-enum Payload {
-    /// Payload for `type_id == 4`
-    Literal(u64),
-    /// Payload for `type_id` other than `type_id == 4`
-    Operation(Vec<Packet>),
-}
-
 impl Packet {
-    /// Parses the packet from the [`InputStream`].
-    /// If successful, this method returns the number of bits read and the packet itself.
+    /// Parses the packet by consuming from the [`InputStream`].
+    /// If successful, this method returns the number of bits read from the stream
+    /// as well as the packet object itself.
     fn from_stream<R: Read>(stream: &mut InputStream<R>) -> anyhow::Result<(usize, Self)> {
         let version = decimal_from_bits(stream.fetch::<3>()?.as_slice());
         let type_id = decimal_from_bits(stream.fetch::<3>()?.as_slice());
-        let (nbits_read, payload) = if type_id == 4 {
-            Payload::parse_literal(stream)?
-        } else {
-            Payload::parse_ops(stream)?
+        let (nbits_read, payload) = match type_id {
+            4 => Payload::parse_literal(stream)?,
+            _ => Payload::parse_ops(stream)?,
         };
         let packet = Packet {
             version,
@@ -109,10 +101,35 @@ impl Packet {
         };
         Ok((nbits_read + 6, packet))
     }
+
+    /// Reduces the packet tree structure into a single value.
+    /// The reducer function (`func`) must compute the reduced value for this packet
+    /// based on the following two input arguments:
+    /// -  The packet itself, and
+    /// -  The slice of reduced values from each subpacket.
+    fn reduce<T, F>(&self, func: &F) -> T
+    where
+        F: Fn(&Self, &[T]) -> T,
+    {
+        let children: Vec<_> = match &self.payload {
+            Payload::Literal(_) => Vec::new(),
+            Payload::Operation(subpackets) => subpackets.iter().map(|sp| sp.reduce(func)).collect(),
+        };
+        func(self, children.as_slice())
+    }
+}
+
+/// Payload of the [`Packet`]
+#[derive(Debug, Clone)]
+enum Payload {
+    /// Payload of the [`Packet`] with `type_id == 4`, containing the literal value
+    Literal(u64),
+    /// Payload of the [`Packet`] with `type_id != 4`, containing sequence of sub-packets
+    Operation(Vec<Packet>),
 }
 
 impl Payload {
-    /// Parses the payload as literal value.
+    /// Parses [`Payload::Literal`] by consuming the next few bits from the stream.
     fn parse_literal<R: Read>(stream: &mut InputStream<R>) -> anyhow::Result<(usize, Payload)> {
         let mut bits = Vec::new();
         let mut nbits_read = 0;
@@ -128,8 +145,8 @@ impl Payload {
         Ok((nbits_read, payload))
     }
 
-    /// Parses the payload as an operation packet.
-    /// This method mainly dispatches to subroutine depending on the length type ID.
+    /// Parses [`Payload::Operation`] by consuming the next few bits from the stream.
+    /// This method dispatches to subroutine depending on the length type ID being read next.
     fn parse_ops<R: Read>(stream: &mut InputStream<R>) -> anyhow::Result<(usize, Payload)> {
         let [length_type_id] = stream.fetch()?;
         let (nbits_read, payload) = match length_type_id {
@@ -140,8 +157,9 @@ impl Payload {
         Ok((nbits_read + 1, payload))
     }
 
-    /// Parses the operation packet where the length type ID was 0,
-    /// i.e. the next 15 bits indicate the total length in bits of sub-packets.
+    /// Parses [`Payload::Operation`] by consuming the next few bits from the stream,
+    /// already knowing that the length type ID previously read was 0.
+    /// Hence, the next 15 bits indicate the total length in bits of sub-packets, etc.
     fn parse_ops_by_bit_length<R: Read>(
         stream: &mut InputStream<R>,
     ) -> anyhow::Result<(usize, Payload)> {
@@ -153,12 +171,18 @@ impl Payload {
             nbits_read += nbits_more;
             subpackets.push(subpacket);
         }
-        let payload = Payload::Operation(subpackets);
-        Ok((nbits_read + 15, payload))
+        ensure!(
+            nbits_read == target_length,
+            "too many bits read while parsing subpackets: {} > {}",
+            nbits_read,
+            target_length
+        );
+        Ok((nbits_read + 15, Payload::Operation(subpackets)))
     }
 
-    /// Parses the operation packet where the length type ID was 1,
-    /// i.e. the next 11 bits indicate the number of sub-packets.
+    /// Parses [`Payload::Operation`] by consuming the next few bits from the stream,
+    /// already knowing that the length type ID previously read was 1.
+    /// Hence, the next 11 bits indicate the number of sub-packets.
     fn parse_ops_by_packet_count<R: Read>(
         stream: &mut InputStream<R>,
     ) -> anyhow::Result<(usize, Payload)> {
@@ -170,14 +194,13 @@ impl Payload {
             nbits_read += nbits_more;
             subpackets.push(subpacket);
         }
-        let payload = Payload::Operation(subpackets);
-        Ok((nbits_read + 11, payload))
+        Ok((nbits_read + 11, Payload::Operation(subpackets)))
     }
 }
 
-/// Converts a hexadecimal character into an array of four bits in MSB order,
-/// where each bit is represented by `0` or `1` under type `u8`.
-fn bits_from_hex_char(c: char) -> anyhow::Result<[Bit; 4]> {
+/// Converts a hexadecimal character into an array of four bits in MSB-first order.
+/// Each bit in the output array is represented by `0` or `1`.
+fn bits_from_hex(c: char) -> anyhow::Result<[Bit; 4]> {
     let decimal = c
         .to_digit(16)
         .map(|d| d as u8)
@@ -190,11 +213,12 @@ fn bits_from_hex_char(c: char) -> anyhow::Result<[Bit; 4]> {
     ])
 }
 
-/// Converts a slice of bits in MSB order into an integer.
+/// Converts a sequence of bits arranged in MSB-first order into an integer.
 fn decimal_from_bits<T>(bits: &[Bit]) -> T
 where
     T: PrimInt,
 {
-    bits.iter()
-        .fold(T::zero(), |acc, &bit| acc + acc + T::from(bit).unwrap())
+    bits.iter().fold(T::zero(), |acc, &bit| {
+        T::from(2).unwrap() * acc + T::from(bit).unwrap()
+    })
 }
