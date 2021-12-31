4697e01e4873bbaf73928aa4b8842d7c7d44cb7f
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2022-01-01 01:54:33 +0700

    day19: Done with input parsing

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,10 +1,10 @@
 //! Day 19: Beacon Scanner, Advent of Code 2021  
 //! <https://adventofcode.com/2021/day/19>
-use std::collections::HashMap;
+use std::fmt::{Debug, Formatter};
 use std::io::BufRead;
+use std::str::FromStr;
 
-use anyhow::Context;
-use itertools::Itertools;
+use anyhow::{ensure, Context};
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -15,6 +15,11 @@ fn main() {
     let input_src = InputSrc::from_arg(std::env::args().nth(1).as_deref());
     let input_reader = input_src.get_reader().expect("cannot open file");
     let Input { reports } = Input::from_buffer(input_reader).expect("cannot parse input");
+
+    for (i, r) in reports.iter().enumerate() {
+        eprintln!("=== scanner {} ===", i);
+        eprintln!("{:?}", r);
+    }
 
     // Part 1: TODO
     let p1_answer = 0;
@@ -34,25 +39,34 @@ struct Input {
 impl Input {
     /// Parses program input from buffered reader.
     fn from_buffer(reader: impl BufRead) -> anyhow::Result<Self> {
-        // lazy_static! {
-        let SCANNER_HEADER_RE: Regex = Regex::new(r"(?i)\s*-+\s*scanner\s+(\d+)\s*-+\s*").unwrap();
-        let COORDS_RE: Regex = Regex::new(
-            r"(?x)
-                    \s*(-?\d+)\s*,
-                    \s*(-?\d+)\s*,
-                    \s*(-?\d+)\s*",
-        )
-        .unwrap();
-        // }
-        let mut reports: HashMap<String, ScannerReport> = HashMap::new();
+        lazy_static! {
+            static ref SCANNER_HEADER_RE: Regex =
+                Regex::new(r"(?i)\s*-+\s*scanner\s+(\d+)\s*-+\s*").unwrap();
+        }
+        let mut reports = Vec::new();
         for line in reader.lines() {
             let line = line.context("cannot read a line")?;
-            if let Some(captures) = SCANNER_HEADER_RE.captures(line.as_str()) {
-            } else if let Some(captures) = COORDS_RE.captures(line.as_str()) {
+            if line.trim().is_empty() {
+                continue;
+            } else if let Some(captures) = SCANNER_HEADER_RE.captures(line.as_str()) {
+                let id: usize = captures[1].parse()?;
+                ensure!(
+                    id == reports.len(),
+                    "invalid scanner id: {} but expected {}",
+                    id,
+                    reports.len()
+                );
+                reports.push(Vec::new());
             } else {
+                let current_report = reports.last_mut().with_context(|| {
+                    format!(
+                        "not started with scanner header: '{}'",
+                        line.escape_default()
+                    )
+                })?;
+                current_report.push(line.parse()?)
             }
         }
-        let reports = reports.into_values().collect_vec();
         Ok(Input { reports })
     }
 }
@@ -61,5 +75,35 @@ impl Input {
 type ScannerReport = Vec<Point3D>;
 
 /// Represents an integer position in 3-dimensional space
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct Point3D(i64, i64, i64);
+
+impl FromStr for Point3D {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref COORDS_RE: Regex = Regex::new(
+                r"(?x)
+                    \s*(-?\d+)\s*,
+                    \s*(-?\d+)\s*,
+                    \s*(-?\d+)\s*"
+            )
+            .unwrap();
+        }
+        let captures = COORDS_RE
+            .captures(s)
+            .with_context(|| format!("invalid point format: '{}'", s.escape_default()))?;
+        Ok(Point3D(
+            captures[1].parse()?,
+            captures[2].parse()?,
+            captures[3].parse()?,
+        ))
+    }
+}
+
+impl Debug for Point3D {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.0, self.1, self.2)
+    }
+}
