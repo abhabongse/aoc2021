2907102866938983e65f1e9b568baa71b0d062ff
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2022-01-02 04:37:57 +0700

    day19: Done

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,19 +1,25 @@
 //! Day 19: Beacon Scanner, Advent of Code 2021  
 //! <https://adventofcode.com/2021/day/19>
+use std::collections::{HashSet, VecDeque};
 use std::fmt::Debug;
 use std::io::BufRead;
 
 use anyhow::{anyhow, bail, ensure, Context};
 use itertools::{iproduct, Itertools};
 use lazy_static::lazy_static;
+use num::Zero;
 use regex::Regex;
 
 use aoc2021::argparser::InputSrc;
 use aoc2021::collect_array::CollectArray;
 use aoc2021::vecmat::{CardinalMatrix, CardinalVector};
 
+/// Represents a point in 3-dimensional space
+type VecPoint = CardinalVector<i64, 3>;
+type TransMatrix = CardinalMatrix<i64, 3, 3>;
+
 lazy_static! {
-    static ref CUBE_ROTATIONS: [CardinalMatrix<i64, 3, 3>; 24] = cube_rotations();
+    static ref CUBE_ROTATIONS: [TransMatrix; 24] = cube_rotations();
 }
 
 /// Main program
@@ -22,15 +28,46 @@ fn main() {
     let input_reader = input_src.get_reader().expect("cannot open file");
     let Input { reports } = Input::from_buffer(input_reader).expect("cannot parse input");
 
-    let result = reports[0].rotate_and_align(&reports[1], 3, 1000);
-    eprintln!("{:?}", result);
+    // Reconstruct the locations of scanners and beacons
+    // using the orient and align technique, targeting 12 overlapping beacons
+    let genesis_report = OrientAlignResult {
+        offset: VecPoint::zero(),
+        report: reports[0].clone(),
+    };
+    let mut base_report_queue = VecDeque::from([genesis_report]);
+    let mut remaining = reports[1..].iter().cloned().collect_vec();
+    let mut beacons = HashSet::new();
+    let mut scanners = Vec::new();
 
-    // Part 1: TODO
-    let p1_answer = 0;
+    // Take a base report from the queue and try to
+    // orient and align all other remaining reports if possible
+    while let Some(base_report) = base_report_queue.pop_front() {
+        let mut next_remaining = Vec::new();
+        for report in remaining {
+            if let Some(result) = base_report.report.orient_and_align(&report, 12, 1000) {
+                base_report_queue.push_back(OrientAlignResult {
+                    offset: base_report.offset + result.offset,
+                    report: result.report,
+                })
+            } else {
+                next_remaining.push(report);
+            }
+        }
+        let new_beacons = base_report.report.0.into_iter();
+        beacons.extend(new_beacons.map(|p| p + base_report.offset));
+        scanners.push(base_report.offset);
+        remaining = next_remaining;
+    }
+
+    // Part 1: Count all beacons
+    let p1_answer = beacons.len();
     println!("Part 1 answer: {}", p1_answer);
 
-    // Part 2: TODO
-    let p2_answer = 0;
+    // Part 2: Furthest pair of scanners
+    let p2_answer = iproduct!(scanners.iter().copied(), scanners.iter().copied())
+        .map(|(a, b)| (a - b).norm1())
+        .max()
+        .expect("empty scanner info");
     println!("Part 2 answer: {}", p2_answer);
 }
 
@@ -95,7 +132,7 @@ impl Input {
     /// Attempts to parse a comma-seperated data into a point in 3-dimensional space.
     /// `None` is returned if the line format does not match.
     /// Other kinds of parsing errors will result in `Some(Err(anyhow::Error))`.
-    fn parse_point(s: &str) -> Option<anyhow::Result<Point3D>> {
+    fn parse_point(s: &str) -> Option<anyhow::Result<VecPoint>> {
         lazy_static! {
             static ref COORDS_RE: Regex = Regex::new(
                 r"(?x)
@@ -122,13 +159,9 @@ impl Input {
     }
 }
 
-/// Represents a point in 3-dimensional space
-type Point3D = CardinalVector<i64, 3>;
-type TransMatrix = CardinalMatrix<i64, 3, 3>;
-
 /// Represents a list of beacon positions reported by a scanner
 #[derive(Debug, Clone)]
-struct Report(Vec<Point3D>);
+struct Report(Vec<VecPoint>);
 
 impl Report {
     /// Creates an empty scanner report.
@@ -137,7 +170,7 @@ impl Report {
     }
 
     /// Adds a new report.
-    fn push(&mut self, point: Point3D) {
+    fn push(&mut self, point: VecPoint) {
         self.0.push(point);
     }
 
@@ -149,16 +182,19 @@ impl Report {
 
     /// Attempts to rotate the `other` scanner report and aligns its reported beacons with _this_ scanner.
     /// See details about other function parameters from [`ScannerReport::align`].
-    fn rotate_and_align(
+    fn orient_and_align(
         &self,
         other: &Self,
         beacon_target: usize,
         scanner_range: i64,
-    ) -> Option<(Point3D, Self)> {
+    ) -> Option<OrientAlignResult> {
         for mat in CUBE_ROTATIONS.iter().copied() {
             let modified_other = other.rotate_copy(mat);
             if let Some(offset) = self.align(&modified_other, beacon_target, scanner_range) {
-                return Some((offset, modified_other));
+                return Some(OrientAlignResult {
+                    offset,
+                    report: modified_other,
+                });
             }
         }
         None
@@ -171,7 +207,7 @@ impl Report {
     /// Function argument `scanner_range` specifies the how far into each direction
     /// that each scanner can see all other beacons.
     /// If alignment fails, this function returns `None`.
-    fn align(&self, other: &Self, beacon_target: usize, scanner_range: i64) -> Option<Point3D> {
+    fn align(&self, other: &Self, beacon_target: usize, scanner_range: i64) -> Option<VecPoint> {
         let paired_offsets = iproduct!(self.0.iter().copied(), other.0.iter().copied())
             .map(|(dp, dq)| dp - dq)
             .counts();
@@ -188,7 +224,7 @@ impl Report {
 
     /// Checks if the specific alignment `offset` between two scanners works as it should be,
     /// i.e. the number of overlapping beacons reaches the `beacon_target` with the `scanner_range`.
-    fn check(&self, other: &Self, offset: Point3D, scanner_range: i64) -> bool {
+    fn check(&self, other: &Self, offset: VecPoint, scanner_range: i64) -> bool {
         let fst_set = self.0.iter().copied();
         let fst_set = fst_set
             .filter(|p| (*p - offset).norm_max() <= scanner_range)
@@ -202,6 +238,15 @@ impl Report {
             .collect_vec();
         fst_set == snd_set
     }
+}
+
+/// Result from trying to rotate and align one report over another
+#[derive(Debug, Clone)]
+struct OrientAlignResult {
+    /// Offset of the second scanner from the first scanner
+    offset: VecPoint,
+    /// Report from the second scanner in the same orientation of the first scanner
+    report: Report,
 }
 
 /// Generates all transformation matrix which would rotate
