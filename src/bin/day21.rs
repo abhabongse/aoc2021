a6e0f970c462b48bb3f70a62157ecb766e785c26
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2022-01-17 01:38:48 +0700

    day21: Part 1 done

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -3,7 +3,7 @@
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
-use anyhow::{ensure, Context};
+use anyhow::Context;
 use clap::Parser;
 use lazy_static::lazy_static;
 use regex::Regex;
@@ -15,56 +15,105 @@ use aoc2021::parsing::QuickParse;
 fn main() {
     let cli = Cli::parse();
     let input_reader = BufReader::new(cli.input_reader().expect("cannot open file"));
-    let Input { player_1, player_2 } =
+    let Input { p1_stats, p2_stats } =
         Input::from_buffer(input_reader).expect("cannot parse input");
 
+    eprintln!("Player {} stats: {:?}", p1_stats.id, p1_stats);
+    eprintln!("Player {} stats: {:?}", p2_stats.id, p2_stats);
+
     // Part 1: TODO
-    let p1_answer = 0;
-    println!("Part 1 answer: {}", p1_answer);
+    let part1_answer = {
+        let game = GameState::new(10, 1000, p1_stats.clone(), p2_stats.clone());
+        game.simulate((1..=1000).cycle(), 3)
+    };
+    println!("Part 1 answer: {}", part1_answer);
 
     // Part 2: TODO
-    let p2_answer = 0;
-    println!("Part 2 answer: {}", p2_answer);
+    let part2_answer = 0;
+    println!("Part 2 answer: {}", part2_answer);
 }
 
 /// Program input data
 #[derive(Debug, Clone)]
 struct Input {
-    player_1: u8,
-    player_2: u8,
+    /// Player 1 statistics
+    p1_stats: PlayerStats,
+    /// Player 2 statistics
+    p2_stats: PlayerStats,
 }
 
 impl Input {
     /// Parses program input from buffered reader.
     fn from_buffer(reader: impl BufRead) -> anyhow::Result<Self> {
         let mut lines = reader.lines();
-        let player_1: PlayerInitState = lines
+        let p1_stats: PlayerStats = lines
             .next()
             .context("expected first line input")??
             .parse()?;
-        ensure!(player_1.id == 1);
-        let player_2: PlayerInitState = lines
+        let p2_stats: PlayerStats = lines
             .next()
             .context("expected first line input")??
             .parse()?;
-        ensure!(player_2.id == 2);
-        Ok(Input {
-            player_1: player_1.pos,
-            player_2: player_2.pos,
-        })
+        Ok(Input { p1_stats, p2_stats })
     }
 }
 
-/// Initial state of a player
+/// A state of Dirac Dice game
 #[derive(Debug, Clone)]
-struct PlayerInitState {
-    /// Player ID
-    id: u8,
-    /// Player starting position
-    pos: u8,
+struct GameState {
+    /// Size of the board, with each cell labeling `1` through `board_size`
+    board_size: u64,
+    /// Scores required to win the game
+    goal: u64,
+    /// Player statistics
+    player_stats: [PlayerStats; 2],
 }
 
-impl FromStr for PlayerInitState {
+impl GameState {
+    /// Creates a new game
+    fn new(board_size: u64, goal: u64, p1_stats: PlayerStats, p2_stats: PlayerStats) -> Self {
+        GameState {
+            board_size,
+            goal,
+            player_stats: [p1_stats, p2_stats],
+        }
+    }
+
+    /// Simulate the game with the specified dice rolls until a player wins.
+    /// It returns the product of the losing player's score with the number of times the die is rolled.
+    fn simulate<I>(&self, mut dice_rolls: I, rolls_per_turn: usize) -> u64
+    where
+        I: Iterator<Item = u64>,
+    {
+        let mut next_player = self.player_stats[0].clone();
+        let mut other_player = self.player_stats[1].clone();
+        for turn_count in 1.. {
+            let move_steps: u64 = dice_rolls.by_ref().take(rolls_per_turn).sum();
+            next_player.pos = match (next_player.pos + move_steps) % self.board_size {
+                0 => self.board_size,
+                pos => pos,
+            };
+            next_player.score += next_player.pos;
+
+            if next_player.score >= self.goal {
+                return other_player.score * turn_count * rolls_per_turn as u64;
+            }
+
+            std::mem::swap(&mut next_player, &mut other_player);
+        }
+        unreachable!()
+    }
+}
+
+/// Statistics of a player in the Dirac Dice game
+#[derive(Debug, Clone)]
+struct PlayerStats {
+    id: u8,
+    pos: u64,
+    score: u64,
+}
+
+impl FromStr for PlayerStats {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -75,9 +124,10 @@ impl FromStr for PlayerInitState {
         let captures = PLAYER_INITIAL_STATE
             .captures(s)
             .context("invalid input line format")?;
-        Ok(PlayerInitState {
+        Ok(PlayerStats {
             id: captures[1].quickparse()?,
             pos: captures[2].quickparse()?,
+            score: 0,
         })
     }
 }
